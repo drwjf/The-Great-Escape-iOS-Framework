8c6d85fff780062088b747ca63391ea92fabd918
Jianfeng Wu <dr.jianfeng.wu@gmail.com>
2025-06-25 09:21:22 -0700

    iOS XCFramework

---
+++ TGEiOS/TGEiOS/TGEiOS.h
@@ -0,0 +1,13 @@
+//
+//  TGEiOS.h
+//  TGEiOS
+//
+
+#ifndef BRGiOS_h
+#define BRGiOS_h
+
+#import <TGEiOS/Types.h>
+#import <TGEiOS/TheGreatEscape.h>
+#import <TGEiOS/Spectrum.h>
+
+#endif /* BRGiOS_h */
